341b3cf1ad7ebbb8762f54c2179ec526ba118b0c
Adolfo Agulló Bernabeu <adagber@gmail.com>
2022-02-04 19:56:27 +0100

    cambiamos a la version 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola mundo. Versión 2!'));
+app.get('/', (req, res) => res.send('Hola mundo. Versión 3!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
